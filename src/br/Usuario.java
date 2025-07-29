1690dda2ff437c50674fe071257791fca381e1a3
Guilherme <ariel.gmachado@hotmail.com>
2025-07-29 14:36:56 -0300

    Define os atributos e metodos da classe Usuario

--- src/br/Usuario.java
+++ src/br/Usuario.java
@@ -1,8 +1,35 @@
 package br;
 
-public class Usuario {
+public abstract class Usuario {
+  //ATRIBUTOS APLICAÇÂO DE ENCAPSULAMENTO
   private String nome;
   private int id;
 
+  //CONSTRUTOR
+  public Usuario(String nome, int id) {
+    this.nome = nome;
+    this.id = id;
+  }
+
+  //GETTERS SETTERS
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  //METODOS PUBLICOS APLICAÇÂO DE ABSTRACAO
+  public abstract double calcularMulta(int diasAtraso);
+  public abstract int getDiasEmprestimo();
 
 }
