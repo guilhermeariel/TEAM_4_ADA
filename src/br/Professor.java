cb1a6f8a31dc2239325dc98eb02bd8278c3fd6c5
Guilherme <ariel.gmachado@hotmail.com>
2025-07-29 15:17:25 -0300

    Implementa a classe professor

--- src/br/Professor.java
+++ src/br/Professor.java
@@ -1,5 +1,32 @@
 package br;
 
-public class Professor {
+public class Professor extends Usuario{
+  //APLICACAO DE ATRIBUTOS ESPECILIZADOS
+  private String departamento;
 
+  //CONSTRUTOR
+  public Professor(String nome, int id, String departamento) {
+    super(nome, id);
+    this.departamento = departamento;
+  }
+
+  //GETTERS E SETTER
+  public String getDepartamento() {
+    return departamento;
+  }
+
+  public void setDepartamento(String departamento) {
+    this.departamento = departamento;
+  }
+
+
+  @Override
+  public double calcularMulta(int diasAtraso) {
+    return diasAtraso * 0.5;
+  }
+
+  @Override
+  public int getDiasEmprestimo() {
+    return 20;
+  }
 }
