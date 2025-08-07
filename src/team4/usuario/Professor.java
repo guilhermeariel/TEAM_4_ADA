41aa246e6b7587d9fbc4f8917c49c034bfb2cd86
JRibaSJr <junior.rib@gmail.com>
2025-08-06 21:34:12 -0300

    Correcores nas classes Professor e ProfessorEstagiario
    # Conflicts:
    #	src/team4/usuario/Professor.java

--- src/team4/usuario/Professor.java
+++ src/team4/usuario/Professor.java
@@ -4,65 +4,57 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Professor extends Usuario {
-  private static final List<String> DEPARTAMENTOS_VALIDOS = Arrays.asList("Matemática", "Informática",
-      "Letras", "Física", "História", "Medicina");
+    private static final List<String> DEPARTAMENTOS_VALIDOS = Arrays.asList(
+            "Matemática", "Informática", "Letras", "Física", "História", "Medicina");
 
-  private String departamento;
-<<<<<<< HEAD:src/br/Professor.java
-  private String nivel; // Ex: mestre, doutor
-  private boolean emAula ;
-=======
-  private String nivel; // ex: mestre, doutor
-  private boolean emAula;
->>>>>>> c09ac29bae84e7b8f6d7f90a9ae0863350e5d165:src/team4/usuario/Professor.java
+    private String departamento;
+    private String nivel; // Ex: mestre, doutor
+    private boolean emAula;
 
-  // Alterado: remoção do parâmetro id (gerado automaticamente pela superclasse)
-  // Adicionado: validação do departamento com base em lista permitida
-  public Professor(String nome, String email, String telefone, String departamento) {
-    super(nome, email, telefone);
+    public Professor(String nome, String email, String telefone, String departamento) {
+        super(nome, email, telefone);
 
-    if (!DEPARTAMENTOS_VALIDOS.contains(departamento)) {
-      throw new IllegalArgumentException("Departamento inválido: " + departamento);
+        if (!DEPARTAMENTOS_VALIDOS.contains(departamento)) {
+            throw new IllegalArgumentException("Departamento inválido: " + departamento);
+        }
+
+        this.departamento = departamento;
+        this.nivel = "mestre";
+        this.emAula = false;
     }
 
-    this.departamento = departamento;
-    this.nivel = "mestre";
-    this.emAula = false;
-  }
+    public String getDepartamento() {
+        return departamento;
+    }
 
-  public String getDepartamento() {
-    return departamento;
-  }
+    public String getNivel() {
+        return nivel;
+    }
 
-  public String getNivel() {
-    return nivel;
-  }
+    public void iniciarAula() {
+        emAula = true;
+    }
 
-  public void iniciarAula() {
-    emAula = true;
-  }
+    public void encerrarAula() {
+        emAula = false;
+    }
 
-  public void encerrarAula() {
-    emAula = false;
-  }
+    public boolean estaDisponivelParaRetirada() {
+        return !emAula && !isBloqueado();
+    }
 
-  public boolean estaDisponivelParaRetirada() {
-    return !emAula && !isBloqueado();
-  }
+    @Override
+    public boolean podePegarEmprestimo() {
+        return estaDisponivelParaRetirada(); // Corrigido aqui
+    }
 
-  @Override
-  public double calcularMulta(int diasAtraso) {
-    return diasAtraso * 0.5;
-  }
+    @Override
+    public double calcularMulta(int diasAtraso) {
+        return diasAtraso * 0.5;
+    }
 
-  @Override
-  public int getDiasEmprestimo() {
-    return 14;
-  }
-
-  @Override
-  public boolean podePegarEmprestimo() {
-    return false;
-  }
+    @Override
+    public int getDiasEmprestimo() {
+        return 14;
+    }
 }
-
--- src/team4/usuario/ProfessorEstagiario.java
+++ src/team4/usuario/ProfessorEstagiario.java
@@ -2,22 +2,22 @@ package team4.usuario;
 
 public class ProfessorEstagiario extends Professor {
 
-  public ProfessorEstagiario(String nome, String email, String telefone, String departamento) {
-    super(nome, email, telefone, departamento);
-  }
+    public ProfessorEstagiario(String nome, String email, String telefone, String departamento) {
+        super(nome, email, telefone, departamento);
+    }
 
-  @Override
-  public double calcularMulta(int diasAtraso) {
-    return diasAtraso * 0.5;
-  }
+    @Override
+    public double calcularMulta(int diasAtraso) {
+        return diasAtraso * 0.5;
+    }
 
-  @Override
-  public int getDiasEmprestimo() {
-    return 7;
-  }
+    @Override
+    public int getDiasEmprestimo() {
+        return 7;
+    }
 
-  @Override
-  public boolean podePegarEmprestimo() {
-    return estaDisponivelParaRetirada(); // torna explícito o comportamento
-  }
+    @Override
+    public boolean podePegarEmprestimo() {
+        return estaDisponivelParaRetirada(); // torna explícito o comportamento
+    }
 }
